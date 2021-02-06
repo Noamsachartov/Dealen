7e3de2e6caf6a23b88eac6c746f61c96a26eb93a
Noamsachartov <>
2021-02-06 22:23:38 +0200

    preferences

--- clientside-enduser/assets/Components/LogInSection/ChoosePreferences.js
+++ clientside-enduser/assets/Components/LogInSection/ChoosePreferences.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput,TouchableOpacity, Button, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity, Button, FlatList, Dimensions, TouchableWithoutFeedback, Switch } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+import Slider from '@react-native-community/slider'
+import SkipIcon from 'react-native-vector-icons/AntDesign';
 import { CategoryData } from '../MainTab/Recommendation-section/Category/CategoryData'
 import TabControler from '../MainTab/TabControler';
 import ImagesPicker from './ImagePicker';
+import { color } from 'react-native-reanimated';
 const { width, height } = Dimensions.get('window')
 
 export default class ChoosePreferences extends React.Component {
@@ -29,7 +32,14 @@ export default class ChoosePreferences extends React.Component {
         console.log(item.title,"added")
       }
 
-      
+      handleSkip = () => {
+        const { navigation } = this.props;
+        navigation.navigate('Login')
+      }
+
+      onValueChange(value) {
+        this.setState({ radios: value });
+      }
 
     componentDidMount =() => {
       this.setState({CategoryDatas: CategoryData})
@@ -41,23 +51,35 @@ export default class ChoosePreferences extends React.Component {
     if(this.state.CategoryDatas){
       return(
         <View style={styles.container}>
-            <Text style={styles.Logo}>נשמח להכיר אותך</Text>
+             <View style={{flex:1, flexDirection:'column' , alignItems: 'flex-end', marginTop: 15}}>
+                 <View>
+                   <TouchableWithoutFeedback onPress={this.handleSkip}>
+                    <SkipIcon size={30} name="arrowright" color="whitesmoke"/>
+                    </TouchableWithoutFeedback>
+                    <Text style={{color: 'whitesmoke', marginHorizontal: 4}}>דלג</Text>
+                    
+                 </View> 
+                 <View style={{flex:1}}>
+               <Text style={styles.Logo}>נשמח להכיר אותך</Text>
+             </View>  
+            </View>
+          
             <FlatList
-            style={{width: width,  maxHeight: 200}}
-            data={this.state.CategoryDatas}
-            renderItem={({ item }) => {
-                return (<View style={styles.inputView} >
-                            <TouchableOpacity onPress={() => this.newCategoryPreferences(item)}>
-                                <Text style={styles.inputText}>{item.title}</Text>
-                            </TouchableOpacity>
-                        </View>)
+              style={{width: width,  maxHeight: 200}}
+              data={this.state.CategoryDatas}
+              renderItem={({ item }) => {
+                  return (<View style={styles.inputView} >
+                              <TouchableOpacity onPress={() => this.newCategoryPreferences(item)}>
+                                  <Text style={styles.inputText}>{item.title}</Text>
+                              </TouchableOpacity>
+                          </View>)
             }}
             keyExtractor={(item, index) => 'key' + index}
             horizontal={false}
             numColumns={4}
             showsHorizontalScrollIndicator={false}
             />
-            <View style={{flex:1,width: width, flexDirection: 'row-reverse'}}>
+            <View style={{flex:0.6,width: width, flexDirection: 'row-reverse'}}>
                 <View style={{flex:1, flexDirection: 'row', justifyContent: 'flex-end'}}>
                     <View >
                         <Text style={styles.inputText}>מסעדה</Text>
@@ -98,10 +120,22 @@ export default class ChoosePreferences extends React.Component {
                     </View>
                </View>
             </View>
+            <View style={{flex:0.4}}>
+                 <Slider 
+                  minimumValue={1}
+                  maximumValue={100}
+                  width={width-20}
+                  minimumTrackTintColor="#fb5b5a"
+                  thumbTintColor="whitesmoke"
+                  step={1}
+                  value={50}
+                  onValueChange={value => this.onValueChange(value)}
+                 />
+            </View>
             <View>
-                <Button 
-                title="דלג"
-                />
+              <TouchableOpacity style={{flex: 0.24, backgroundColor: "#465881", borderRadius: 10, width: width/2, justifyContent: 'center', alignItems: 'center'}}>
+                 <Text>להתחברות</Text>
+              </TouchableOpacity>
             </View>
         </View>
       )
@@ -117,8 +151,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#003f5c',
-        alignItems: 'center',
-        
+        alignItems: 'center', 
         width: width
       },
       inputView:{
@@ -139,7 +172,6 @@ const styles = StyleSheet.create({
         fontWeight:"bold",
         fontSize:40,
         color:"#fb5b5a",
-        marginVertical: 40
       },
 
 });
