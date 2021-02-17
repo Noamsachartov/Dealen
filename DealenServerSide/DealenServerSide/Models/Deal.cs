774bd528c32f8ffffa19ab58772c3e87dfdf89cc
ulevin8844 <>
2021-02-17 13:03:00 +0200

    ד

--- DealenServerSide/DealenServerSide/Models/Deal.cs
+++ DealenServerSide/DealenServerSide/Models/Deal.cs
@@ -12,11 +12,13 @@ namespace DealenServerSide.Models
         string name;
         string description;
         string business_Name;
+        int business_id;
         string category;
         DateTime startime;
         DateTime endtime;
         string image; 
         int cat_id;
+        double discount;
 
         public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
@@ -27,9 +29,12 @@ namespace DealenServerSide.Models
         public DateTime Endtime { get => endtime; set => endtime = value; }
         public string Image { get => image; set => image = value; }
         public int Cat_id { get => cat_id; set => cat_id = value; }
+        public int Business_id { get => business_id; set => business_id = value; }
+        public double Discount { get => discount; set => discount = value; }
 
+        public Deal() { }
 
-        public Deal(int id, string name, string description, string business_Name, string category, DateTime startime, DateTime endtime, string image, int cat_id)
+        public Deal(int id, string name, string description, string business_Name, string category, DateTime startime, DateTime endtime, string image, int cat_id, int business_id, double discount)
         {
             Id = id;
             Name = name;
@@ -40,10 +45,9 @@ namespace DealenServerSide.Models
             Endtime = endtime;
             Image = image;
             Cat_id = cat_id;
+            Business_id = business_id;
+            Discount = discount;
         }
-        public Deal() { }
-
-
 
         public List<Deal> Read()
         {
