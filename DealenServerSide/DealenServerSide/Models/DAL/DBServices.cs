49adc122c77abc8f6e9063f7d1df768aa7ad28b1
MaliItzhak <77485819+MaliItzhak@users.noreply.github.com>
2021-03-10 15:04:49 +0200

    Update DBServices.cs

--- DealenServerSide/DealenServerSide/Models/DAL/DBServices.cs
+++ DealenServerSide/DealenServerSide/Models/DAL/DBServices.cs
@@ -765,16 +765,11 @@ public class DBServices
 
             StringBuilder sb = new StringBuilder();
 
-            sb.AppendFormat("SELECT  CASE WHEN d.description LIKE '%Letter%' THEN d.description" +
-                             "WHEN  d.name LIKE '%Letter%' THEN  d.name" +
-                             "WHEN c.name LIKE '%Letter%' THEN  c.name" +
-                             "WHEN b.bdescription LIKE '%Letter%' THEN b.bdescription" +
-                             "WHEN b.bname LIKE '%Letter%' THEN b.bname" +
-                             "ELSE ''"+
-                             "END"+
-                " FROM Businesses_2021 AS b INNER JOIN dealInbus_2021 AS db ON b.bid = db.business_id INNER JOIN Deal_2021 AS d ON" +
-                " db.deal_id = d.id INNER JOIN Category_2021 AS c ON d.cat_id = c.id");
-                 
+            sb.AppendFormat("SELECT db.id, b.bname, db.business_id as business_id,db.startime, db.endtime, db.discount, c.name AS catgeory_name, d.image, d.description, d.name AS deal_name, d.cat_id as cat_id" +
+                            "FROM Businesses_2021 AS b INNER JOIN dealInbus_2021 AS db ON b.bid = db.business_id INNER JOIN Deal_2021 AS d ON" +
+                            "db.deal_id = d.id INNER JOIN Category_2021 AS c ON d.cat_id = c.id" +
+                            "WHERE b.bname LIKE 'Letter%' OR d.name LIKE 'Letter%' OR c.name LIKE 'Letter%'");
+
             selectSTR = sb.ToString();
 
 
@@ -786,21 +781,20 @@ public class DBServices
             {   // Read till the end of the data into a row
                 Deal d = new Deal();
                 d.Id = Convert.ToInt32(dr["id"]);
+                d.Name = (string)dr["deal_name"];
                 d.Business_Name = (string)dr["bname"];
                 d.Business_id = Convert.ToInt32(dr["business_id"]);
                 d.Category = (string)dr["catgeory_name"];
+                d.Cat_id = Convert.ToInt32(dr["cat_id"]);
                 d.Startime = (TimeSpan)dr["startime"];
                 d.Endtime = (TimeSpan)dr["endtime"];
                 d.Image = (string)dr["image"];
                 d.Description = (string)dr["description"];
                 d.Discount = Convert.ToInt32(Convert.ToDouble(dr["discount"]) * 100);
-                d.Name = (string)dr["deal_name"];
-                d.Coupon = Convert.ToInt32(dr["coupon"]);
+                dlist.Add(d);
                 //string starttimeString24Hour = Convert.ToDateTime(context.Request.QueryString["starttime"]).ToString("HH:mm", CultureInfo.CurrentCulture);
                 //string endtimeString24Hour = Convert.ToDateTime(context.Request.QueryString["endtime"]).ToString("HH:mm", CultureInfo.CurrentCulture);
                 //edit.endtime = endtimeString24Hour;
-                dlist.Add(d);
-
             }
 
             return dlist;
@@ -820,6 +814,71 @@ public class DBServices
         }
 
     }
+    //List<Deal> dlist = new List<Deal>();
+    //SqlConnection con = null;
+
+    //string selectSTR = null;
+    //try
+    //{
+    //    con = connect("DBConnectionString"); // create a connection to the database using the connection String defined in the web config file
+
+    //    StringBuilder sb = new StringBuilder();
+
+    //    sb.AppendFormat("SELECT  CASE WHEN d.description LIKE '%Letter%' THEN d.description" +
+    //                     "WHEN  d.name LIKE '%Letter%' THEN  d.name" +
+    //                     "WHEN c.name LIKE '%Letter%' THEN  c.name" +
+    //                     "WHEN b.bdescription LIKE '%Letter%' THEN b.bdescription" +
+    //                     "WHEN b.bname LIKE '%Letter%' THEN b.bname" +
+    //                     "ELSE ''"+
+    //                     "END"+
+    //        " FROM Businesses_2021 AS b INNER JOIN dealInbus_2021 AS db ON b.bid = db.business_id INNER JOIN Deal_2021 AS d ON" +
+    //        " db.deal_id = d.id INNER JOIN Category_2021 AS c ON d.cat_id = c.id");
+
+    //    selectSTR = sb.ToString();
+
+
+    //    SqlCommand cmd = new SqlCommand(selectSTR, con);
+
+    //    // get a reader
+    //    SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection); // CommandBehavior.CloseConnection: the connection will be closed after reading has reached the end
+    //    while (dr.Read())
+    //    {   // Read till the end of the data into a row
+    //        Deal d = new Deal();
+    //        d.Id = Convert.ToInt32(dr["id"]);
+    //        d.Business_Name = (string)dr["bname"];
+    //        d.Business_id = Convert.ToInt32(dr["business_id"]);
+    //        d.Category = (string)dr["catgeory_name"];
+    //        d.Startime = (TimeSpan)dr["startime"];
+    //        d.Endtime = (TimeSpan)dr["endtime"];
+    //        d.Image = (string)dr["image"];
+    //        d.Description = (string)dr["description"];
+    //        d.Discount = Convert.ToInt32(Convert.ToDouble(dr["discount"]) * 100);
+    //        d.Name = (string)dr["deal_name"];
+    //        d.Coupon = Convert.ToInt32(dr["coupon"]);
+    //        //string starttimeString24Hour = Convert.ToDateTime(context.Request.QueryString["starttime"]).ToString("HH:mm", CultureInfo.CurrentCulture);
+    //        //string endtimeString24Hour = Convert.ToDateTime(context.Request.QueryString["endtime"]).ToString("HH:mm", CultureInfo.CurrentCulture);
+    //        //edit.endtime = endtimeString24Hour;
+    //        dlist.Add(d);
+
+    //    }
+
+    //    return dlist;
+    //}
+    //catch (Exception ex)
+    //{
+    //    // write to log
+    //    throw (ex);
+    //}
+    //finally
+    //{
+    //    if (con != null)
+    //    {
+    //        con.Close();
+    //    }
+
+    //}
+
+
 
     //פונקציה קבלת פרטי הקטגוריה-Dealen
 
