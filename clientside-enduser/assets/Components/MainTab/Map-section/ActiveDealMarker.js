cf9f642ca479c9bb71fd5fb64ac8b8bdb3492dd6
ulevin8844 <77484427+ulevin8844@users.noreply.github.com>
2021-03-19 16:14:04 +0200

    ss
    ss

---
+++ clientside-enduser/assets/Components/MainTab/Map-section/ActiveDealMarker.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity , Image,ImageBackground, Dimensions, TouchableWithoutFeedback,FlatList} from 'react-native';
+const { width, height } = Dimensions.get('window');
+import DealOnMapItem from'./DealOnMapItem';
+
+
+
+export default class ActiveDealMarker extends React.Component {
+
+    state={
+      isLoading: true
+
+    }
+
+
+  componentDidMount = () => {
+    }
+     
+  render(){
+    if(this.props.data){
+      return(
+        <FlatList
+        data={this.props.data}
+        renderItem={({ item }) => {
+            return <DealOnMapItem UserData={this.props.UserData} item={item} key={item.Id}/> 
+        }}
+        keyExtractor={(item, index) => 'key' + index}
+        scrollEventThrottle={16}
+        decelerationRate={"fast"}
+    />
+ 
+      )
+    }else {
+      return (
+      <View>
+          <Text>Loading..</Text>
+      </View>
+      );
+  }
+  }
+}
+
