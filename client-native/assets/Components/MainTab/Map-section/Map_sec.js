2737e36e6a915fc6091759466e2882ee6a33bd32
Noamsachartov <>
2021-03-28 19:41:12 +0300

    Update Map_sec.js

--- client-native/assets/Components/MainTab/Map-section/Map_sec.js
+++ client-native/assets/Components/MainTab/Map-section/Map_sec.js
@@ -3,25 +3,12 @@ import { Text, View, ScrollView, SafeAreaView,StatusBar } from 'react-native';
 import Map from './Map';
 import { FlatList } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
-import { LogBox } from 'react-native';
 
-
-LogBox.ignoreLogs([
- 'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead',
-]);
 
 export default function Map_sec() {
   console.log("inside recommendation")
   const navigation = useNavigation(); 
     return (
-      <View>  
-          {/* <StatusBar
-          animated={true}
-          backgroundColor="#003f5c"
-           /> */}
-          {/* <ScrollView showsVerticalScrollIndicator={false} > */}
-            <Map navigation={navigation} />  
-          {/* </ScrollView> */}
-      </View>
+      <Map navigation={navigation} />  
     );
   }
