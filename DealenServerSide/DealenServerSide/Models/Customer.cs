df89b80bbd0c5ea9831d5e7235bd4588bdc4e2b3
Noamsachartov <>
2021-03-31 15:44:34 +0300

    update customer preferences

--- DealenServerSide/DealenServerSide/Models/Customer.cs
+++ DealenServerSide/DealenServerSide/Models/Customer.cs
@@ -18,6 +18,9 @@ namespace DealenServerSide.Models
         string password;
         //System.Data.Spatial.DbGeography location;
         string image;
+        string p_category;
+        string p_type;
+        string p_distance;
 
 
         public int Cust_id { get => cust_id; set => cust_id = value; }
@@ -29,10 +32,13 @@ namespace DealenServerSide.Models
         public string Password { get => password; set => password = value; }
         public string Image { get => image; set => image = value; }
         public string Cust_lname { get => cust_lname; set => cust_lname = value; }
+        public string P_category { get => p_category; set => p_category = value; }
+        public string P_type { get => p_type; set => p_type = value; }
+        public string P_distance { get => p_distance; set => p_distance = value; }
 
         //internal Spatial.DbGeography Location { get => location; set => location = value; }
 
-        public Customer(int cust_id, string cust_fname, string cust_lname, string cust_address, string cust_phone, DateTime birthdate, string cust_mail, string password, string image/*, Spatial.DbGeography location*/)
+        public Customer(int cust_id, string cust_fname, string cust_lname, string cust_address, string cust_phone, DateTime birthdate, string cust_mail, string password, string image, string p_category, string p_type, string p_distance/*, Spatial.DbGeography location*/)
         {
             Cust_id = cust_id;
             Cust_fname = cust_fname;
@@ -43,6 +49,10 @@ namespace DealenServerSide.Models
             Cust_mail = cust_mail;
             Password = password;
             Image = image;
+            P_category = p_category;
+            P_type = p_type;
+            P_distance = p_distance;
+            
             //Location = location;
         }
 
@@ -60,7 +70,11 @@ namespace DealenServerSide.Models
         {
             DBServices dbs = new DBServices();
             return dbs.Insert(this);
-
+        }
+        public int UpdateIntialPreferences(int id, Customer customer)
+        {
+            DBServices dbs = new DBServices();
+            return dbs.UpdateIntialPreferences(id, customer);
         }
     }
 }
