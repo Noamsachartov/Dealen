d35a511a423ec297ce6a6519c35329ae0dce14ec
ulevin8844 <77484427+ulevin8844@users.noreply.github.com>
2021-04-19 00:06:19 +0300

    ע
    ע

--- clientside-business-owner/src/Components/MainTab/Home/Advertising_Deal.jsx
+++ clientside-business-owner/src/Components/MainTab/Home/Advertising_Deal.jsx
@@ -79,11 +79,86 @@ getTags=()=>{
   onImageChange = event => {
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
+  onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      var data = new FormData();
+      let img = event.target.files[0];
+      console.log(img)
       this.setState({
         image: URL.createObjectURL(img)
       });
     }
   };
+    console.log(this.state.image)
+  };
+
+//   onImageChange = (event) => {
+       
+
+//     console.log(event.target.files[0]);
+//     var data = new FormData();
+//     if (event.target.value.length > 0) {
+
+//       var is_logged = localStorage.getItem("user_id") ? localStorage.getItem("user_id") : 0;
+//       is_logged = JSON.parse(is_logged);
+
+//         //this.setState({ selectedFile: event.target.files[0].name });
+//         const file = event.target.files[0];
+//         console.log(file);
+//         const newUrl = URL.createObjectURL(file);
+//         console.log(newUrl);
+//         this.setState({ imgURL: newUrl })
+
+//         data.append("UploadedImage", file);
+//         data.append("name", is_logged);
+
+//         console.log("in post img function");
+
+//         //this.apiUrl = `http://localhost:54976/api/User/uploadedFiles`;
+
+//         this.apiUrl = `http://proj.ruppin.ac.il/igroup49//test2/tar1/api/UploadedFiles/uploadedFiles`;
+
+//         fetch(this.apiUrl,
+//             {
+//                 method: 'POST',
+//                 body: data,
+//                 // headers: new Headers({
+//                 //   // 'Content-Type': 'application/json; charset=UTF-8',
+//                 //   // 'Accept': 'application/json; charset=UTF-8'
+//                 // })
+//             })
+//             .then(res => {
+//                 console.log('res=', res);
+
+//                 if (res.status === 201) {
+//                     console.log('uploadedFile created:)');
+//                 }
+//                 console.log('res.ok', res.ok);
+
+//                 if (res.ok) {
+//                     console.log('post succeeded');
+//                 }
+
+//                 return res.json()
+//             })
+//             .then(
+//                 (result) => {
+//                     console.log("fetch btnFetchuploadedFile= ", result);
+//                     let imgNameInServer = result.split('\\').pop();
+//                     console.log(imgNameInServer);
+//                     this.setState({ urlimg: result, selectedFile: imgNameInServer })
+
+//                 },
+//                 (error) => {
+//                     console.log("err post=", error);
+//                 });
+//         console.log('end');
+//     }
+//     else {
+//         this.setState({ selectedFile: null })
+//     }
+// }
+
 
 
   mySubmitHandler = (event) => {
@@ -127,6 +202,9 @@ getTags=()=>{
 
     alert("You are submitting " + this.state.username);
     console.log(this.state);
+    // alert("You are submitting " + is_logged);
+    
+    console.log(jbody);
   }
   myChangeHandler = (event) => {
     this.setState({username: event.target.value});
