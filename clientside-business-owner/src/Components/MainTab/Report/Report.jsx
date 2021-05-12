dd51d6c6fbc63ed8f30d51b444358272eb0d6888
MaliItzhak <77485819+MaliItzhak@users.noreply.github.com>
2021-05-12 19:24:10 +0300

    Report

--- clientside-business-owner/src/Components/MainTab/Report/Report.jsx
+++ clientside-business-owner/src/Components/MainTab/Report/Report.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import BlogOverview from './BlogOverview'
 
 export default class Report extends Component {
     render() {
         return (
             <div>
                 <h3>Report</h3>
+                <BlogOverview />
             </div>
         )
     }
